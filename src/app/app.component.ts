c1335104e7b90cbd22b1c68ea1e222026a91afaa
Darth Jar Jar <fabiocarrico1999@gmail.com>
2024-08-12 17:45:03 +0100

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+
+let imports = [
+  RouterOutlet,
+  NavbarComponent,
+  HeroComponent
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [imports],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AppleWebsiteClone';
 }
+
+
