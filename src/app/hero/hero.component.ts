76e326eb992ed523a2bb5066c62754b0e83102ae
Darth Jar Jar <fabiocarrico1999@gmail.com>
2024-08-15 15:28:46 +0100

    iPhone VIdeo section

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ViewChild } from '@angular/core';
+
+import { gsap } from 'gsap-trial';
 
 @Component({
   selector: 'app-hero',
@@ -7,6 +9,44 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit{
+
+  @ViewChild('section') elementView: any;
+
+  @ViewChild('video') videoElement: any;
+
+  public previousVideoPath: string = "";
+
+  constructor(){ }
+
+  ngAfterViewInit(): void {
+    this.videoSelector()
+    this.setupGsap()
+  }
+
+
+  videoSelector() {
+    if(this.elementView.nativeElement.offsetWidth > 640){
+      if(this.previousVideoPath != "/videos/hero.mp4"){
+        this.videoElement.nativeElement.src = "/videos/hero.mp4";
+        this.previousVideoPath = "/videos/hero.mp4";
+      }
+    }else{
+      if(this.previousVideoPath != "/videos/smallHero.mp4"){
+        this.videoElement.nativeElement.src = "/videos/smallHero.mp4";
+        this.previousVideoPath = "/videos/smallHero.mp4";
+      }
+    }
+  }
+
+  setupGsap(): void {
+    gsap.to('#hero', {
+      opacity: 1,
+      duration: 2
+    });
+
+
+
+  }
 
 }
