be950828f0dde07f55085fe532d3639d7cd1d212
Darth Jar Jar <fabiocarrico1999@gmail.com>
2024-08-17 15:10:36 +0100

    Video carousel component creation

---
+++ src/app/video-carousel/video-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-carousel.component.html',
+  styleUrl: './video-carousel.component.css'
+})
+export class VideoCarouselComponent {
+
+}
